350cbaff8ffc112aa24f84ecff11b28efd0b1e79
weiyiming <997990179@email.com>
2025-12-23 04:49:38 +0700

    update:补充swagger页面说明和修正swagger配置

--- mestools-admin/src/main/java/com/mes/web/core/config/SwaggerConfig.java
+++ mestools-admin/src/main/java/com/mes/web/core/config/SwaggerConfig.java
@@ -114,9 +114,9 @@ public class SwaggerConfig {
         // 用ApiInfoBuilder进行定制
         return new ApiInfoBuilder()
                 // 设置标题
-                .title("标题：若依管理系统_接口文档")
+                .title("标题：MesTools_接口文档")
                 // 描述
-                .description("描述：用于管理集团旗下公司的人员信息,具体包括XXX,XXX模块...")
+                .description("描述：打开F12从后台请求头中复制authorization的值粘贴到Authorize打开后的value中,打开目标接口点击Try it out 即可使用")
                 // 作者信息
                 .contact(new Contact(mesToolsConfig.getName(), null, null))
                 // 版本
